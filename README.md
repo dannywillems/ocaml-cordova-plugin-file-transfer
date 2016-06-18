55e6c28c282231bb5605594d93112e84a3d91062
Danny Willems <contact@danny-willems.be>
2016-06-18 14:59:34 +0200

    Functions

---
+++ README.md
@@ -0,0 +1,59 @@
+# ocaml-cordova-plugin-file-transfer
+
+[![LGPL-v3 licensed](https://img.shields.io/badge/license-LGPLv3-blue.svg)](https://raw.githubusercontent.com/dannywillems/ocaml-cordova-plugin-file-transfer/master/LICENSE)
+[![Build Status](https://travis-ci.org/dannywillems/ocaml-cordova-plugin-file-transfer.svg?branch=master)](https://travis-ci.org/dannywillems/ocaml-cordova-plugin-file-transfer)
+
+Binding to
+[cordova-plugin-file-transfer](https://github.com/apache/cordova-plugin-file-transfer)
+
+[Example
+application](https://github.com/dannywillems/ocaml-cordova-plugin-file-transfer-example).
+
+## What does cordova-plugin-file-transfer do ?
+
+```
+This plugin allows you to upload and download files.
+
+This plugin defines global `FileTransfer`, `FileUploadOptions` constructors. Although in the global scope, they are not available until after the `deviceready` event.
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+        console.log(FileTransfer);
+    }
+```
+
+Source: [cordova-plugin-file-transfer](https://github.com/apache/cordova-plugin-file-transfer)
+
+## How to install and compile your project by using this plugin ?
+
+Don't forget to switch to a compiler **>= 4.03.0**.
+```Shell
+opam switch 4.03.0
+```
+
+* If you added
+[ocaml-cordova-plugin-list](https://github.com/dannywillems/ocaml-cordova-plugin-list)
+as opam package provider, you can use
+```
+opam install cordova-plugin-file-transfer
+```
+
+* Else, you can use opam by pinning the repository with
+```Shell
+opam pin add cordova-plugin-file-transfer https://github.com/dannywillems/ocaml-cordova-plugin-activityindicator.git
+```
+
+To compile your project, use
+```Shell
+ocamlfind ocamlc -c -o [output_file] -package gen_js_api -package cordova-plugin-file-transfer [...] -linkpkg [other arguments]
+```
+
+Don't forget to install cordova-plugin-file-transfer with
+```Shell
+cordova plugin add cordova-plugin-file-transfer
+```
+
+## How to use ?
+
+See the official documentation
+[cordova-plugin-file-transfer](https://github.com/apache/cordova-plugin-file-transfer)
---
+++ cordova_file_transfer.mli
@@ -0,0 +1,126 @@
+(* -------------------------------------------------------------------------- *)
+type t = private Ojs.t
+
+val create : unit -> t
+[@@js.new "FileTransfer"]
+
+(* -------------------------------------------------------------------------- *)
+(* Ocaml types and methods for upload method. Use them to have
+ * type checking at compile time *)
+(* HTTP_METHOD *)
+type http_method  =
+  | Post [@js "Post"]
+  | Put [@js "Put"]
+  [@@js.enum]
+
+(* File Transfer Error code *)
+type transfer_error_code =
+  | File_not_found [@js 1]
+  | Invalid_url [@js 2]
+  | Connection [@js 3]
+  | Abort [@js 4]
+  | Not_modified [@js 5]
+  [@@js.enum]
+
+type transfer_error = private Ojs.t
+
+(* One of the predefined error codes listed above. *)
+val code          : transfer_error -> transfer_error_code
+[@@js.get "code"]
+
+(* URL to the source. *)
+val source              : transfer_error -> string
+
+(* URL to the target. *)
+val target              : transfer_error -> string
+
+(* HTTP status code. This attribute is only available when a response code
+ * is received from the HTTP connection. *)
+val http_status         : transfer_error -> int option
+
+(* Response body. This attribute is only available when a response is
+ * received from the HTTP connection. *)
+val body                : transfer_error -> string option
+
+(* Either e.getMessage or e.toString *)
+val ext                 : transfer_error -> string
+[@@js.get "exception"]
+(* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
+type upload_options = private Ojs.t
+
+val create_upload_options :
+  ?file_key:(string [@js.default "file"])                   ->
+  ?file_name:(string [@js.default "image.jpg"])             ->
+  ?http_method:(http_method [@js.default Post])             ->
+  ?mime_type:(string [@js.default "image/jpeg"])            ->
+  ?params:Ojs.t                                             ->
+  ?chunked_mode:(bool [@js.default true])                   ->
+  ?headers:Ojs.t array                                      ->
+  unit                                                      ->
+  upload_options
+[@@js.builder]
+(* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
+type upload_result = private Ojs.t
+
+(* The number of bytes sent to the server as part of the upload. Not supported
+ * on iOS *)
+val bytes_sent          : upload_result -> int
+
+(* The HTTP response code returned by the server. Not supported on iOS *)
+val response_code       : upload_result -> int
+
+(* The HTTP response returned by the server. *)
+val response            : upload_result -> string
+
+(* The HTTP response headers by the server. Only iOS is supported *)
+val headers             : upload_options -> Ojs.t
+(* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
+(* Sends a file to a server *)
+(* upload [url] [server_adress] [success_callback] [error_callback]
+ * [options] [trust_all_hosts] *)
+val upload :
+  t                                           ->
+  string                                      ->
+  string                                      ->
+  (upload_result -> unit)                     ->
+  (transfer_error -> unit)                    ->
+  ?options:upload_options                    ->
+  ?true_all_hosts:(bool [@js.default false])  ->
+  unit                                        ->
+  unit
+[@@js.call]
+(* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
+type entry = private Ojs.t
+
+val to_url : entry -> string
+[@@js.get]
+
+(* Other fields ? *)
+
+(* Downloads a file from server *)
+val download :
+  t                                                                     ->
+  string                                                                ->
+  string                                                                ->
+  (entry -> unit)                                                       ->
+  (transfer_error -> unit)                                              ->
+  ?true_all_hosts:(bool [@js.default false])                            ->
+  (* Download options ? *)
+  unit                                                                  ->
+  unit
+[@@js.call]
+
+(* Aborts an in-progress transfer *)
+val abort :
+  t -> unit
+[@@js.call]
+
+(* -------------------------------------------------------------------------- *)
